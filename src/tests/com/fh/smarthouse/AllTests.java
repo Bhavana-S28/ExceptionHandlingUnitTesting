4fe5e6af838aa08a6575aeb0c42fcac1244b713b
bhava <bhava@172.22.206.73>
2024-12-02 23:23:08 +0100

    Exception handling unit testing changes

--- src/tests/com/fh/smarthouse/AllTests.java
+++ src/tests/com/fh/smarthouse/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ EnergyManagerTest.class, SmartObjectSimulatorTest.class })
+@SelectClasses({ DataExchangeSimulatorTest.class, LogFileCreatorTest.class })
 public class AllTests {
 
 }
